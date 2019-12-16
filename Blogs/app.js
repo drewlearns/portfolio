08afcd67c0692fe15502b306f77ab7871899ba80
DrewLearns <drew@drewlearns.com>
2019-12-15 22:54:14 -0500

    Finished yelpcamp v2 and started on blog.app

---
+++ Blogs/app.js
@@ -0,0 +1,72 @@
+//APP CONFIG
+const express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose")
+//MONGOOSE SETUP
+mongoose.connect("mongodb://localhost/restful-blog-app", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+//BODY-PARSER (BOILERPLATE)
+app.use(bodyParser.urlencoded({ extended: true }));
+//MAKES IT SO I DON'T NEED TO TYPE .ejs IN THE PATH WITHIN APP.JS
+app.set("view engine", "ejs");
+//SERVES CUSTOM STYLES SHEET
+app.use(express.static('public'));
+//CUSTOM CONNECTION READOUT
+app.listen(process.env.PORT, process.env.IP, () => {
+    console.log(`
+       	**********************************************************
+	       __                  __                          
+	  ____/ /_________      __/ /___  ____ ____  __   ____
+	 / __  / ___/ _ \\ | /| / / / _ \\/ __ / ___// __ \\/ ___/
+	/ /_/ / /  /  __/ |/ |/ / /  __/ /_/ / /  / / / (__  ) 
+	\\__,_/_/   \\___/|__/|__/_/\\___/\\__,_/_/  /_/ /_/____/
+	https://DrewLearns.com | Drew@drewlearns.com
+
+	*********************************************************
+	Node/Express Server Started - Now listening on respective Port.
+    `)
+});
+
+//MONGOOSE MODEL CONFIG 
+let blogSchema = new mongoose.Schema({
+    title: String,
+    image: {
+            type: String, 
+            default: 'https://drewlearns.com/wp-content/uploads/2019/12/Image-2019-12-14-at-01.25.20.png'
+            },
+    body: String,
+    create: {
+            type: Date, 
+            default: Date.now()
+            }
+});
+let Blog = mongoose.model("Blog", blogSchema);
+
+//RESTFUL ROUTES (CRUD) -- (create, read, update, delete)
+
+app.get('/blogs', (req, res) => {
+    Blog.find({}, (err, blogs) => {
+        err ? console.log('ERROR!') : res.render('index', {blogs : blogs});
+    })
+})
+app.get('/', (req, res) => {
+    res.redirect('/blogs');
+})
+//**************************************************************************************************
+// RESTFUL BLOGS COMMENTED OUT - FOR TESTING PURPOSES ONLY
+// Blog.create({
+//     title: 'Test Blog',
+//     image: 'https://commons.wikimedia.org/wiki/File:Wien_Deutschordenskirche_Innenraum_01.jpg',
+//     body: `
+    
+//     This Gothic church was built in the 14th century (1326–1375) and consecrated to St. Elisabeth of Hungary. Some of the stucco work was performed by the Italian artists Simone Allio in 1697 and Girolamo Alfieri in 1700. The church was remodelled in Baroque style in 1720 (probably) by the architect Anton Erhard Martinelli, while Count Guido von Starhemberg was the commander of the Order. Alfieri worked again in this church in the period 1720–1725, as well as the sculptor Giovanni Antonio Canevale. However, the church has retained some of its Gothic origins, such as pointed arches in the windows.
+
+//     The walls are decorated with rows of numerous armorial bearings of the Order of Teutonic Knights and several commemorative stones, such as the tombstone of Count Siegfried Sarau with relief work by Giovanni Stanetti and of bailiff Jobst von Wetzhausen (1524) by Loy Hering.
+
+//     Of particular interest is the Flemish winged triptych, a polychromed altarpiece from 1520. The woodcarver and the painter are unknown. The polychromy was made and signed by Jan van Wavere, a polychromer from Mechelen.[4] It depicts in vivid woodcarvings scenes from the Passion of Christ.`
+// });
+// console.log('Be sure to comment out THIS SECTION from app.js to avoid duplicated db entries!')
+//**************************************************************************************************
--- yelpcamp_v2/app.js
+++ yelpcamp_v2/app.js
@@ -1,95 +1,75 @@
-var express     = require("express"),
-    app         = express(),
-    bodyParser  = require("body-parser"),
-    mongoose    = require("mongoose")
+const express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose")
 
 mongoose.connect("mongodb://localhost/yelp_camp", {
-    useNewUrlParser: true, 
-    useUnifiedTopology: true 
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 });
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
 // SCHEMA SETUP
-var campgroundSchema = new mongoose.Schema({
-   name: String,
-   image: String,
-   description: String
+let campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String
 });
 
-var Campground = mongoose.model("Campground", campgroundSchema);
+let Campground = mongoose.model("Campground", campgroundSchema);
 
-Campground.create(
-     {
-         name: "Granite Hill", 
-         image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg",
-         description: "This is a huge granite hill, no bathrooms.  No water. Beautiful granite!"
-         
-     },
-     function(err, campground){
-      if(err){
-          console.log(err);
-      } else {
-          console.log("NEWLY CREATED CAMPGROUND: ");
-          console.log(campground);
-      }
+Campground.create({
+        name: "Granite Hill",
+        image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg",
+        description: "This is a huge granite hill, no bathrooms.  No water. Beautiful granite!"
+
+    },
+    (err, campground) => {
+        (err ? console.log(err) : console.log("NEWLY CREATED CAMPGROUND: "));
+        (err ? console.log(err) : console.log(campground));
     });
 
-    
-app.get("/", function(req, res){
+app.get("/", (req, res) => {
     res.render("landing");
 });
 
 //INDEX - show all campgrounds
-app.get("/campgrounds", function(req, res){
+app.get("/campgrounds", (req, res) => {
     // Get all campgrounds from DB
-    Campground.find({}, function(err, allCampgrounds){
-       if(err){
-           console.log(err);
-       } else {
-          res.render("index",{campgrounds:allCampgrounds});
-       }
+    Campground.find({}, (err, allCampgrounds) => {
+        err ? console.log(err) : res.render("index", { campgrounds: allCampgrounds });
     });
 });
 
 //CREATE - add new campground to DB
-app.post("/campgrounds", function(req, res){
+app.post("/campgrounds", (req, res) => {
     // get data from form and add to campgrounds array
-    var name = req.body.name;
-    var image = req.body.image;
-    var desc = req.body.description;
-    var newCampground = {name: name, image: image, description: desc}
+    let name = req.body.name;
+    let image = req.body.image;
+    let desc = req.body.description;
+    let newCampground = { name: name, image: image, description: desc }
     // Create a new campground and save to DB
-    Campground.create(newCampground, function(err, newlyCreated){
-        if(err){
-            console.log(err);
-        } else {
-            //redirect back to campgrounds page
-            res.redirect("/campgrounds");
-        }
+    Campground.create(newCampground, (err, newlyCreated) => {
+        err ? console.log(err) : res.redirect("/campgrounds");
     });
 });
 
 //NEW - show form to create new campground
-app.get("/campgrounds/new", function(req, res){
-   res.render("new.ejs"); 
+app.get("/campgrounds/new", (req, res) => {
+    res.render("new.ejs");
 });
 
 // SHOW - shows more info about one campground
-app.get("/campgrounds/:id", function(req, res){
+app.get("/campgrounds/:id", (req, res) => {
     //find the campground with provided ID
-    Campground.findById(req.params.id, function(err, foundCampground){
-        if(err){
-            console.log(err);
-        } else {
-            //render show template with that campground
-            res.render("show", {campground: foundCampground});
-        }
+    Campground.findById(req.params.id, (err, foundCampground) => {
+        err ? console.log(err) : res.render("show", { campground: foundCampground });
     });
-})
+});
 
-app.listen(process.env.PORT, process.env.IP, function(){
-   console.log(`
+app.listen(process.env.PORT, process.env.IP, () => {
+    console.log(`
    	**********************************************************
 	       __                  __                          
 	  ____/ /_________      __/ /___  ____ ____  __   ____
