7606737f323ec2e82965e3153ee7d575251db277
DrewLearns <drew@drewlearns.com>
2019-12-16 22:58:26 -0500

    Express CRUD - Finished index, new, create, show.

--- blog_app/app.js
+++ blog_app/app.js
@@ -1,8 +1,8 @@
 //APP CONFIG
-const express = require("express"),
-    app = express(),
-    bodyParser = require("body-parser"),
-    mongoose = require("mongoose")
+const   bodyParser  = require("body-parser"),
+        express     = require("express"),
+        app         = express(),
+        mongoose    = require("mongoose")
 //MONGOOSE SETUP
 mongoose.connect("mongodb://localhost/restful-blog-app", {
     useNewUrlParser: true,
@@ -18,10 +18,10 @@ app.use(express.static('public'));
 app.listen(process.env.PORT, process.env.IP, () => {
     console.log(`
        	**********************************************************
-	       __                  __                          
+	       __                  __
 	  ____/ /_________      __/ /___  ____ ____  __   ____
 	 / __  / ___/ _ \\ | /| / / / _ \\/ __ / ___// __ \\/ ___/
-	/ /_/ / /  /  __/ |/ |/ / /  __/ /_/ / /  / / / (__  ) 
+	/ /_/ / /  /  __/ |/ |/ / /  __/ /_/ / /  / / / (__  )
 	\\__,_/_/   \\___/|__/|__/_/\\___/\\__,_/_/  /_/ /_/____/
 	https://DrewLearns.com | Drew@drewlearns.com
 
@@ -30,38 +30,66 @@ app.listen(process.env.PORT, process.env.IP, () => {
     `)
 });
 
-//MONGOOSE MODEL CONFIG 
+//MONGOOSE MODEL CONFIG
 let blogSchema = new mongoose.Schema({
     title: String,
     image: {
-            type: String, 
+            type: String,
             default: 'https://drewlearns.com/wp-content/uploads/2019/12/Image-2019-12-14-at-01.25.20.png'
             },
     body: String,
     create: {
-            type: Date, 
+            type: Date,
             default: Date.now()
-            }
+            },
+    description: String
 });
 let Blog = mongoose.model("Blog", blogSchema);
 
 //RESTFUL ROUTES (CRUD) -- (create, read, update, delete)
-
+// - CREATE
+    // --"NEW" ROUTE
+app.get('/blogs/new', (req, res) => {
+    res.render('new');
+});
+    // -- CREATE BLOG ROUTE
+app.post('/blogs', (req, res) => {
+        res.render('new');
+        // -- REDIRECT NEW POST BACK TO INDEX
+    Blog.create( req.body.blog, (err, newBlog) => {
+        err ? (res.render('/new')) : (res.redirect('/blogs', {blog : newBlog}));
+    });
+    });
+// - SHOW
+app.get('/blogs/:id', (req, res) => {
+    Blog.findById(req.params.id, (err, foundBlog) => {
+        err ? (res.redirect('/blogs')) : (res.render('show', {blog : foundBlog}))
+    ;})
+});
+// - READ
 app.get('/blogs', (req, res) => {
-    Blog.find({}, (err, blogs) => {
+    Blog.find({}, (err, blogs, description) => {
         err ? console.log('ERROR!') : res.render('index', {blogs : blogs});
     })
 })
+
+// - UPDATE
+// - DELETE
+
+
+
+// REDIRECTS
 app.get('/', (req, res) => {
     res.redirect('/blogs');
-})
+});
+
 //**************************************************************************************************
 // RESTFUL BLOGS COMMENTED OUT - FOR TESTING PURPOSES ONLY
 // Blog.create({
-//     title: 'Test Blog',
+//     title: 'Gothic church',
 //     image: 'https://commons.wikimedia.org/wiki/File:Wien_Deutschordenskirche_Innenraum_01.jpg',
 //     body: `
-    
+
 //     This Gothic church was built in the 14th century (1326–1375) and consecrated to St. Elisabeth of Hungary. Some of the stucco work was performed by the Italian artists Simone Allio in 1697 and Girolamo Alfieri in 1700. The church was remodelled in Baroque style in 1720 (probably) by the architect Anton Erhard Martinelli, while Count Guido von Starhemberg was the commander of the Order. Alfieri worked again in this church in the period 1720–1725, as well as the sculptor Giovanni Antonio Canevale. However, the church has retained some of its Gothic origins, such as pointed arches in the windows.
 
 //     The walls are decorated with rows of numerous armorial bearings of the Order of Teutonic Knights and several commemorative stones, such as the tombstone of Count Siegfried Sarau with relief work by Giovanni Stanetti and of bailiff Jobst von Wetzhausen (1524) by Loy Hering.
