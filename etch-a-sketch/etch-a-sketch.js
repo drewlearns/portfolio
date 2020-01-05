c7fba9e47e11f3019151365893a0b3df6e1d839d
Drew K <drew@drewlearns.com>
2020-01-05 00:40:01 -0500

    Some JS created

---
+++ etch-a-sketch/etch-a-sketch.js
@@ -0,0 +1,16 @@
+// SELECT THE ELEMENTS ON THE PAGE -Canvas, SHAKE
+const canvas = document.querySelector('#etch-a-sketch');
+console.log(canvas);
+const ctx = canvas.getContext('2d');
+const shakeButton = document.querySelector(`.shake`);
+// SETUP OUR CANVAS FOR DRAWING
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+ctx.lineWidth = 10;
+// WRITE  DRAW Function
+
+// WRITE A HANDLER FOR THE MSMediaKeySession
+
+// CLEAR "SHAKE" Function
+
+// LISTEN FOR ARROW KEYS
