561ada8ba4887c5cde781d22f234a557743c37d8
Drew K <drew@drewlearns.com>
2020-01-05 01:13:33 -0500

    before destructuring width/height

--- etch-a-sketch/etch-a-sketch.js
+++ etch-a-sketch/etch-a-sketch.js
@@ -4,9 +4,17 @@ console.log(canvas);
 const ctx = canvas.getContext('2d');
 const shakeButton = document.querySelector(`.shake`);
 // SETUP OUR CANVAS FOR DRAWING
+const width = canvas.width;
+const height = canvas.height;
+console.log(width, height);
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
 ctx.lineWidth = 10;
+ctx.beginPath(); //STARTS THE DRAWING (PUTS PEN ON PAPER)
+ctx.moveTo(200, 200);
+ctx.lineTo(200, 200);
+ctx.stroke(); 
+
 // WRITE  DRAW Function
 
 // WRITE A HANDLER FOR THE MSMediaKeySession
