bcb8f7f0938da0b2f452a6652bcf7d3d3c5c0c34
Drew K <drew@drewlearns.com>
2020-01-19 13:53:06 -0500

    errors everywhere - pre debugging commit

---
+++ gallery refactored/gallery.js
@@ -0,0 +1,102 @@
+function Gallery(gallery) {
+        console.log(Gallery);
+        if (!gallery) {
+                throw new Error("No Gallery Found!");
+        };
+        this.gallery = gallery;
+        // SELECT ELEMENTS WE NEED
+        this.images = Array.from(gallery.querySelectorAll("img"));
+        console.log(images);
+        this.modal = document.querySelector(".modal");
+        this.prevButton = this.modal.querySelector(".prev");
+        this.nextButton = this.modal.querySelector(".next");
+        //BINDING METHODS 
+        this.showNextImage = this.showNextImage.bind(this);
+        this.handleKeyUp = this.handleKeyUp.bind(this);
+        this.showPrevImage = this.showPrevImage.bind(this);
+        this.handleClickOutside = this.handleClickOutside.bind(this);
+        // IMAGE EVENT LISTENERS
+        this.images.forEach(image =>
+                image.addEventListener("click", event => this.showImage(event.currentTarget))
+        );
+        // KEY EVENT LISTENERS
+        this.images.forEach(image => {
+                image.addEventListener("keyup", event => {
+                        if (event.key === "Enter") {
+                                this.showImage(event.currentTarget);
+                        };
+                });
+        });
+        this.modal.addEventListener("click", this.handleClickOutside);
+};
+
+//USE IT ON THE PAGE
+
+//MAKE A FUNCTION THAT OPENS THE MODAL
+Gallery.prototype.openModal = function () {
+        //DONT FORGET TO INITIATE THIS AT THE BOTTOM OF THE FILE
+        console.info('Opening Modal');
+        //First check if the modal is already open because animations
+        if (this.modal.matches(".open")) {
+                console.info("New Modal is open");
+                return; //stops the function from running
+        };
+        this.modal.classList.add("open");
+        // console.log(currentImage);
+        //EVENT LISTENERS TO BE BOUND WHEN OPENING
+        window.addEventListener("keyup", (event) => this.handleKeyUp(event));
+        this.nextButton.addEventListener("click", () => this.showNextImage());
+        this.prevButton.addEventListener("click", () => this.showPrevImage());
+};
+//CLOSE MODAL KICKED OFF BY A HANDLER
+Gallery.prototype.closeModal = function () {
+        console.info('Closing Modal');
+        this.modal.classList.remove("open");
+        //ADD EVENT LISTENERS FOR CLICKS & KEYBOARD
+        window.removeEventListener("keyup", this.handleKeyUp);
+        this.nextButton.removeEventListener("click", this.showNextImage);
+        this.prevButton.removeEventListener("click", this.showPrevImage);
+};
+//HANDLE CLICKING TO CLOSE MODAL
+Gallery.prototype.handleClickOutside = function () {
+        if (event.target === event.currentTarget) {
+                this.closeModal();
+        };
+};
+//HANDLE KEYUP TO CLOSE MODAL, NEXT & PREVIOUS
+Gallery.prototype.handleKeyUp = function () {
+        if (event.key === "Escape") return this.closeModal();
+        if (event.key === "ArrowRight") return this.showNextImage();
+        if (event.key === "ArrowLeft") return this.showPrevImage();
+};
+
+//NEXT BUTTON
+Gallery.prototype.showNextImage = function () {
+        console.log(currentImage.nextElementSibling);
+        console.log(gallery.firstElementChild);
+        this.showImage(this.currentImage.nextElementSibling || this.gallery.firstElementChild);
+};
+Gallery.prototype.showPrevImage = function () {
+        console.log(currentImage.prevElementSibling);
+        console.log(gallery.lasElementChild);
+        this.showImage(this.currentImage.previousElementSibling || this.gallery.lastElementChild);
+};
+//SHOW IMAGE
+Gallery.prototype.showImage= function (el) {
+        if (!el) {
+                console.log("No image to show");
+                return;
+        };
+        //update the modal with this information
+        console.log(el);
+        this.modal.querySelector("img").src = el.src; //updates the image source
+        console.log(el.src);
+        this.modal.querySelector("h2").textContent = el.title; //updates the image source
+        console.log(el.title);
+        this.modal.querySelector("figure p").textContent = el.dataset.description; //updates the image source
+        console.log(el.dataset.description);
+        this.currentImage = el;
+        this.openModal();
+};
+const gallery1 = Gallery(document.querySelector(".gallery1"));
+// const gallery2 = Gallery(document.querySelector(".gallery2"));
