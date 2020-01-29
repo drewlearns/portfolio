bf820fa7caba6a677341e3ecb2b642aee9bf7d95
Drew K <drew@drewlearns.com>
2020-01-28 19:58:24 -0500

    currency created

---
+++ currency/scripts.js
@@ -0,0 +1,64 @@
+const fromSelect = document.querySelector('[name="from_currency"]');
+const toSelect = document.querySelector('[name="to_currency"]');
+const endpoint = 'https://api.exchangeratesapi.io/latest';
+const ratesByBase = {};
+const currencies = {
+  USD: 'United States Dollar',
+  AUD: 'Australian Dollar',
+  BGN: 'Bulgarian Lev',
+  BRL: 'Brazilian Real',
+  CAD: 'Canadian Dollar',
+  CHF: 'Swiss Franc',
+  CNY: 'Chinese Yuan',
+  CZK: 'Czech Republic Koruna',
+  DKK: 'Danish Krone',
+  GBP: 'British Pound Sterling',
+  HKD: 'Hong Kong Dollar',
+  HRK: 'Croatian Kuna',
+  HUF: 'Hungarian Forint',
+  IDR: 'Indonesian Rupiah',
+  ILS: 'Israeli New Sheqel',
+  INR: 'Indian Rupee',
+  JPY: 'Japanese Yen',
+  KRW: 'South Korean Won',
+  MXN: 'Mexican Peso',
+  MYR: 'Malaysian Ringgit',
+  NOK: 'Norwegian Krone',
+  NZD: 'New Zealand Dollar',
+  PHP: 'Philippine Peso',
+  PLN: 'Polish Zloty',
+  RON: 'Romanian Leu',
+  RUB: 'Russian Ruble',
+  SEK: 'Swedish Krona',
+  SGD: 'Singapore Dollar',
+  THB: 'Thai Baht',
+  TRY: 'Turkish Lira',
+  ZAR: 'South African Rand',
+  EUR: 'Euro',
+};
+
+function generateOptions(options){
+        return Object.entries(options)
+                .map(
+                ([currencyCode, currencyName]) =>
+                        `<option value="${currencyCode}">${currencyCode} - ${currencyName}</option>`
+                )
+                .join('');
+};
+
+async function fetchRates(base = 'USD'){
+        const response = await fetch(endpoint)
+        const rates = await response.json();
+        return rates;
+};
+
+function convert(amount, from, to){
+        //CHECK TO SEE IF WE HAVE THE RATES TO CONVERT FROM FIRST
+        if (!rattesByBase[from]){
+                console.log('Oh no, we don\'t have the ${from} to convert to ${to}. So let\'s go get it!');
+        };
+};
+
+const optionsHTML = generateOptions(currencies);
+fromSelect.innerHTML = optionsHTML;
+toSelect.innerHTML = optionsHTML;
